8dd5ef5c355bbfee7bf4074bdc5b18f42392e519
red-sprout <jjw05015@gmail.com>
2023-12-26 21:37:44 +0900

    todo list - 업로드만 구현

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,24 +1,33 @@
-import {useState, useEffect} from "react";
-
-function Hello() {
-  function byeFn() {
-    console.log("bye :(");
-  }
-  function hiFn(){
-    console.log("hi :)");
-    return byeFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Write your to do...">
+        </input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
