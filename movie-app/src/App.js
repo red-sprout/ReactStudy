3759b3ce49f2f4ca759fdde6ee5774963148877c
red-sprout <jjw05015@gmail.com>
2023-12-26 14:17:01 +0900

    movie app

---
+++ movie-app/src/App.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react";
+
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn(){
+    console.log("hi :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
