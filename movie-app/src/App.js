23e3c502c200881498c5670853f313a21108cf1a
red-sprout <jjw05015@gmail.com>
2023-12-29 16:59:41 +0900

    url 추가

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,32 @@
 import {
   BrowserRouter as Router,
+  Switch,
   Route,
 } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
   return <Router>
-    <Route>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
       <Route path="/">
         <Home />
       </Route>
-    </Route>
+    </Switch>
   </Router>;
 }
+/*
+react-router-dom 5버전 -> 버전6 바뀐 부분
 
+1. Switch컴포넌트가 Routes컴포넌트로 대체되었습니다.
+Switch -> Routes
+
+2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀌었습니다.
+Route path="/" element={< Home / >}
+
+react-router-dom 6버전 문서
+https://reactrouter.com/docs/en/v6/getting-started/overview
+*/
 export default App;
