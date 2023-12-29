c2974b5c5faaae775a12f8ef7385e6e8bb5b4feb
red-sprout <jjw05015@gmail.com>
2023-12-29 16:29:54 +0900

    movie-app

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,32 +1,16 @@
-import {useState, useEffect} from "react";
-
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [])
-  return (
-    <div>
-      <h1>The coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}   
-    </div>
-  );
+  return <Router>
+    <Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Route>
+  </Router>;
 }
 
 export default App;
